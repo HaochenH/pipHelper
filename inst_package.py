c5ba79bf005f788b6a18acd348f1748fe7f12d3b
Haochen Hu <hhuuucc@Hotmail.com>
2022-12-08 15:01:59 -0800

    update readme.md

--- inst_package.py
+++ inst_package.py
@@ -4,6 +4,7 @@ def inst_package(p_name):
     """
     Install package(s) inside a python script.
     Supported formats:
+    inst_package("numpy")
     inst_package("numpy, requests, pandas")
     inst_package(["numpy", "requests", "pandas"])
     """
---
+++ readme.md
@@ -0,0 +1,5 @@
+Install package(s) inside a python script.
+Supported formats:
+inst_package("numpy")
+inst_package("numpy, requests, pandas")
+inst_package(["numpy", "requests", "pandas"])

5d2e77c3002dc348e72b6a7cd54211543a541875
Haochen Hu <hhuuucc@Hotmail.com>
2022-12-08 15:19:10 -0800

    error handling

--- inst_package.py
+++ inst_package.py
@@ -1,5 +1,6 @@
 import subprocess
 
+
 def inst_package(p_name):
     """
     Install package(s) inside a python script.
@@ -9,20 +10,25 @@ def inst_package(p_name):
     inst_package(["numpy", "requests", "pandas"])
     """
 
-    if isinstance(p_name, str):  # if p_name is a string, install the package
-        print(f'\033[35mInstalling {p_name}...')
-        if "," in p_name:
-            p_name = p_name.split(",")
-            inst_package(p_name)
-            return
-        p = p_name.replace(" ", "")
-        print(f'Checking status of {p} using pip...\033[0m')
-        output = subprocess.run("pip3 show " + p, shell=True)
-        if output.returncode != 0:
-            subprocess.run("pip3 install " + p, shell=True)
-        else:
-            print(f'{p} is already installed, skipping installation.\n')
+    try:
+        if isinstance(p_name, str):  # if p_name is a string, install the package
+            print(f'\033[35mInstalling {p_name}...')
+            if "," in p_name:
+                p_name = p_name.split(",")
+                inst_package(p_name)
+                return
+            p = p_name.replace(" ", "")
+            print(f'Checking status of {p} using pip...\033[0m')
+            output = subprocess.run("pip3 show " + p, shell=True)
+            if output.returncode != 0:
+                subprocess.run("pip3 install " + p, shell=True)
+            else:
+                print(f'{p} is already installed, skipping installation.\n')
 
-    if isinstance(p_name, list):  # if p_name is a list, install all packages in the list
-        for p in p_name:
-            inst_package(p)
+        if isinstance(p_name, list):  # if p_name is a list, install all packages in the list
+            for p in p_name:
+                inst_package(p)
+
+    except Exception as e:
+        print(f'\033[31mAn error {e} occurred while installing {p_name}.\033[0m')
+
--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
 # inst_package
-### Install package(s) inside a python script. 
+### Install package(s) inside a python script
+[![Python](https://img.shields.io/badge/python-3)](https://www.python.org/downloads/)
 
 Supported formats: 
 
@@ -8,3 +9,9 @@ inst_package("numpy")
 inst_package("numpy, requests, pandas") 
 inst_package(["numpy", "requests", "pandas"])
 ```
+
+inst_package install one or more Python packages using the `pip` command. The function accepts a single parameter called p_name, which can be either a string or a list of strings containing the name(s) of the package(s) to be installed.
+
+The `inst_package()` function first checks the type of the p_name parameter using the isinstance() function. If p_name is a string, the function checks if the string contains a comma character. If it does, the string is split into a list of strings using the split() method, and then the function is called recursively with the list of strings as the argument.
+
+> It can be used in python programs which need dependencies that are not installed by default. I wrote it for my personal interest because I wanted to run a script on a new environment without the need to manually install any dependencies.

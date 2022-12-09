1ac33fc82fda1a1c2585cd8e6856a8ca8c4218e3
Haochen Hu <hhuuucc@Hotmail.com>
2022-12-08 16:41:39 -0800

    add to example

--- example_use.py
+++ example_use.py
@@ -5,3 +5,7 @@ inst_package.inst("numpy")
 inst_package.inst("numpy, requests, pandas")
 # or
 inst_package.inst(["numpy", "requests", "pandas"])
+# install a specific version of a package
+inst_package.inst_version("numpy", "1.19.2")
+inst_package.inst_version(["numpy", "requests", "pandas"], ["1.19.2", "2.28.1", "1.2.3"])
+inst_package.inst_version("numpy, requests, pandas", "1.19.2, 2.28.1, 1.2.3")
